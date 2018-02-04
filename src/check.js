64491a36c93f2180f36a34c3ce46acc983cf6b7c
Simon CORNAZ <scornaz@e2r2p14.42.fr>
2018-02-04 11:35:01 +0100

    add_runi_better_algo

--- src/check.js
+++ src/check.js
@@ -1,10 +1,14 @@
 export default function checkSolution() {
 	let flag = 0
+	console.log("ta grosse mere")
 	this.vals.forEach(function(val, index) {
-		if (this.sol[index] == val) {
+		if (this.sol[flag] == val) {
 			flag += 1
+        } else flag = + (this.sol[0] == val)
+        if (flag == this.sol.length){
+            this.win = 1
+            console.log('win')
+            return
 		}
 	}.bind(this))
-	if (flag == this.sol.length)
-		this.win = 1
 }
